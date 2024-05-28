c45a4ae50918915163502763b2263d3a26f447bf
kimhaerinring <rlagofls0824@naver.com>
2024-05-28 12:26:50 +0900

    StudyGame

--- src/com/sun/study1/Study9_1.java
+++ src/com/sun/study1/Study9_1.java
@@ -35,12 +35,28 @@ public class Study9_1 {
 			if(flag) {
 				System.out.println("게임시작");
 				int level =1;
+				int mon=3;
+				int gold=0;
+				
 				for(level=1;level<15;level++) {
+					System.out.println("1.사냥 2. 종료");
+					int select =sc.nextInt();
+					if(select!=1) {
+						break;
+					}
+					if(level%5==0) {
+						gold=gold+level/5*1000;
+						System.out.println(level+" 축하금"+level/5*1000+"지급");
+					}
 					System.out.println("현재 레벨 :"+level);
+					for(int j= 0;j<level*mon;j++) {
+						System.out.println(j+1+"번째 사냥");
+					}
 				}
-				System.out.println("최종레벨: "+level);
+				gold=gold+3000;
+				System.out.println("최종레벨: "+level +"최종골드"+gold);
 			}
-			
+			System.out.println("프로그램이 종료 됩니다.");
 			
 			
 			
