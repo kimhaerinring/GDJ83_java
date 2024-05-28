f3a72222edbf5b627d76b8e4e36adbf461b60a21
kimhaerinring <rlagofls0824@naver.com>
2024-05-28 13:54:04 +0900

    Study

---
+++ src/com/sun/study2/S1.java
@@ -0,0 +1,16 @@
+package com.sun.study2;
+
+public class S1 {
+	public static void main(String[] args) {
+		// 배열- 하나의 변수로 어러개의 데이터를 다룰 때 사용
+		// let ar =[1,2,3,'a',true]
+		// 같은 데이터 타입만 모을 수 있다.
+		// 배열 선언
+		// 모을려고 하는 데이터 타입명[ ] 변수명=new 모을려고 하는 데이터 타입명[갯수]
+		int[] a = new int[3];
+		String[] names = new String[2];
+		System.out.println(a);
+		System.out.println(names);
+	}
+
+}
