a8f6cdd6626db600601d048f00bf31d2314ac476
kimhaerinring <rlagofls0824@naver.com>
2024-05-31 09:42:25 +0900

    car

--- src/com/sun/s4/Car.java
+++ src/com/sun/s4/Car.java
@@ -4,7 +4,15 @@ public class Car {
 	// 접근지정자 [그외지정자] 데이터타입 변수명;
 	public static String company;
 
+	public int price;
+
 	public static void info() {
+		System.out.println(Car.company);
 		System.out.println("static method");
+	}
+
+	public void intfo2() {
+		Car.info();
+
 	}
 }
